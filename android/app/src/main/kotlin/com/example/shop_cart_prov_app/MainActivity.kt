a75d2583c645edba30d36d40f1156c96eb42ef74
Binoy <binoyvk99@gmail.com>
2024-03-04 00:17:11 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/shop_cart_prov_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shop_cart_prov_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_cart_prov_app/providers/cart_provider.dart';
+
+import 'pages/cart_page.dart';
+import 'pages/products_page.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (BuildContext context) => CartProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Shop Cart',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: "/products",
+      routes: {
+        "/products": (context) => ProductsPage(),
+        "/cart": (context) => CartPage(),
+      },
+    );
+  }
+}
